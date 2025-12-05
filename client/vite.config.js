a307c1a1aa066a3b579b876324f00ccebb6c847e
KatibAnsariTech <katib.ansari@spacetotech.com>
2025-12-05 20:07:46 +0530

    update link

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://private-ai-assistant-vvya.vercel.app',
         changeOrigin: true,
       },
     },
